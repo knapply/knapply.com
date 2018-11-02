77f49a37b2ceb5a00d93e44681c21a6d956d5c34
knapply <syknapptic@gmail.com>
2018-11-02 16:29:21 -0700

    hello

--- blogdown/post/functions/index_cache/html/unnamed-chunk-1_sourceCpp/sourceCpp-x86_64-w64-mingw32-0.12.19.3/sourcecpp_1d7053443b27/file1d70458362bc.cpp.R
+++
@@ -1,5 +0,0 @@
-`.sourceCpp_1_DLLInfo` <- dyn.load('C:/Users/Windows/Documents/knapply.com/content/post/functions/index_cache/html/unnamed-chunk-1_sourceCpp/sourceCpp-x86_64-w64-mingw32-0.12.19.3/sourcecpp_1d7053443b27/sourceCpp_2.dll')
-
-cpp_doubler <- Rcpp:::sourceCppFunction(function(x) {}, FALSE, `.sourceCpp_1_DLLInfo`, 'sourceCpp_1_cpp_doubler')
-
-rm(`.sourceCpp_1_DLLInfo`)
---
+++ blogdown/post/functions/index_cache/html/unnamed-chunk-1_sourceCpp/sourceCpp-x86_64-w64-mingw32-0.12.19.3/sourcecpp_3f4c5bda2491/file3f4c5fe27321.cpp.R
@@ -0,0 +1,5 @@
+`.sourceCpp_1_DLLInfo` <- dyn.load('C:/Users/Windows/Documents/knapply.com/content/post/functions/index_cache/html/unnamed-chunk-1_sourceCpp/sourceCpp-x86_64-w64-mingw32-0.12.19.3/sourcecpp_3f4c5bda2491/sourceCpp_2.dll')
+
+cpp_doubler <- Rcpp:::sourceCppFunction(function(x) {}, FALSE, `.sourceCpp_1_DLLInfo`, 'sourceCpp_1_cpp_doubler')
+
+rm(`.sourceCpp_1_DLLInfo`)
