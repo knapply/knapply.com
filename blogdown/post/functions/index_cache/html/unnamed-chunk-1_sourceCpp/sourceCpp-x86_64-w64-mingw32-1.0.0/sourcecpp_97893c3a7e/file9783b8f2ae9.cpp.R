ee96888f7fbbcf3c68d6f7e378bd90abd32b32a4
knapply <syknapptic@gmail.com>
2018-11-26 12:47:34 -0800

    mode

---
+++ blogdown/post/functions/index_cache/html/unnamed-chunk-1_sourceCpp/sourceCpp-x86_64-w64-mingw32-1.0.0/sourcecpp_97893c3a7e/file9783b8f2ae9.cpp.R
@@ -0,0 +1,5 @@
+`.sourceCpp_1_DLLInfo` <- dyn.load('C:/Users/Windows/Documents/knapply.com/content/post/functions/index_cache/html/unnamed-chunk-1_sourceCpp/sourceCpp-x86_64-w64-mingw32-1.0.0/sourcecpp_97893c3a7e/sourceCpp_2.dll')
+
+cpp_doubler <- Rcpp:::sourceCppFunction(function(x) {}, FALSE, `.sourceCpp_1_DLLInfo`, 'sourceCpp_1_cpp_doubler')
+
+rm(`.sourceCpp_1_DLLInfo`)
